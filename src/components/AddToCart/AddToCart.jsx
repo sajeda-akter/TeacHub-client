35409138cf71917a4ac8c4db57fc470d595dabb2
Sajeda Akter <sajeda16akter@gmail.com>
2023-12-01 09:12:48 +0600

    'addToProduct-complete'

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -1,10 +1,93 @@
+import Swal from "sweetalert2";
 
 const AddToCart = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const handleAddProduct=(e)=>{
+    e.preventDefault()
+    const form=e.target
+    const name=form.name.value
+    const brand=form.brand.value
+    const rating=form.rating.value
+    const price=form.price.value
+    const type=form.type.value
+    const photoURL=form.photoURL.value
+    const description=form.description.value
+    const newProduct={
+      name,brand,photoURL,type,price,rating,description
+    }
+    fetch('http://localhost:5000/addProduct',{
+      method:"POST",
+      headers:{
+        'content-type':"application/json"
+      },
+      body:JSON.stringify(newProduct)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+ 
+Swal.fire({
+  position: "top-center",
+  icon: "success",
+  title: "Your work has been saved",
+  showConfirmButton: false,
+  timer: 1000
+});
+      console.log(data)
+    })
+  }
+  return (
+    <div>
+      <h1 className="text-4xl text-center font-medium my-4 text-[#FF6969] border-b-4 border-[#FEA1A1] w-72 mx-auto h-12">
+    Add To Product
+      </h1>
+
+      <form
+      onSubmit={handleAddProduct}
+        action=""
+        className="bg-[#FFEBD8] lg:w-2/4 grid grid-cols-2   gap-3 mx-auto p-12 rounded-xl"
+      >
+        <input
+          type="text"
+          placeholder="Enter product name"
+          name="name"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          type="text"
+          placeholder="Enter brand name"
+          name="brand"
+          className="input input-bordered w-full max-w-xs "
+        />
+        <input
+          type="text"
+          placeholder="Enter product photoURL"
+          name="photoURL"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          type="text"
+          placeholder="Enter product type"
+          name="type"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          type="text"
+          placeholder="Product price"
+          name="price"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          type="text"
+          placeholder="Product rating"
+          name="rating"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <textarea name="description" className="textarea textarea-bordered h-32 col-span-2" placeholder="Write product description"></textarea>
+        <button className="btn btn-active btn-primary w-52 mt-4 ml-24 lg:ml-56">ADD</button>
+
+      
+      </form>
+    </div>
+  );
 };
 
 export default AddToCart;
